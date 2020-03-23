cb8aac8f02102a9071030167695da42d4967c59d
Norberto Mantohac <nmantohac82098@gmail.com>
2020-03-22 19:36:22 -0700

    completed clean up of javascript and html

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -3,61 +3,61 @@ var lowercaseCheckBox = document.querySelector("#lowercase-checkbox");
 var uppercaseCheckBox = document.querySelector("#uppercase-checkbox");
 var numbersCheckBox = document.querySelector("#numbers-checkbox");
 var symbolsCheckBox = document.querySelector("#symbols-checkbox");
-// var passwordResult = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
 
-const passwordCriteria = {
+var passwordCriteria = {
   lowercase: "abcdefghijklmnopqrstuvwxyz",
   uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   numbers: "0123456789",
-  symbols: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+  symbols: "!\"#$%&'()*+,-./:;<=>?@[\\\\]^_`{|}~"
 };
 
-generateBtn.addEventListener("click", () => {
-  var length = +passwordLength.value;
+generateBtn.addEventListener("click", function() {
+  var passwordLengthValue = +passwordLength.value;
   var lowercaseActive = lowercaseCheckBox.checked;
   var uppercaseActive = uppercaseCheckBox.checked;
   var numbersActive = numbersCheckBox.checked;
   var symbolsActive = symbolsCheckBox.checked;
 
-  generateRandomPassword(lowercaseActive, uppercaseActive, numbersActive, symbolsActive, length);
+  generateRandomPassword(lowercaseActive, uppercaseActive, numbersActive, symbolsActive, passwordLengthValue);
 });
 
 function generateRandomPassword(lower, upper, num, sym, length) {
-  let mainString = "";
-  let password = "";
+  var mainString = "";
+  var password = "";
 
-  const options = {
+  var options = {
     lowercase: lower,
     uppercase: upper,
     numbers: num,
     symbols: sym
   };
 
-  for (var i = 0; i < Object.keys(options).length; i++) {
-    mainString += (Object.values(options)[i]) ? passwordCriteria[Object.keys(options)[i]] : "";
-  }
+  var objectKeys = Object.keys(options)
+  var objectValues = Object.values(options)
 
-  if (mainString != "" && length >= 8 && length <= 128) {
-    for (var j = 0; j < length; j++) {
-      password += mainString[Math.floor(Math.random() * mainString.length)];
+  for (var i = 0; i < objectKeys.length; i++) {
+    if (objectValues[i]) {
+      mainString += passwordCriteria[objectKeys[i]];
     }
+    else {
+      mainString += "";
+    }
+  }
 
-    document.querySelector("#password").value = password;
+  var confirmPassword = confirm("Are you sure about your choices and wish to proceed?")
+
+  if (confirmPassword) {
+    if (mainString != "" && length >= 8 && length <= 128) {
+      for (var j = 0; j < length; j++) {
+        password += mainString[Math.floor(Math.random() * mainString.length)];
+      }
+      
+      document.querySelector("#password").value = password;
+    }
+    else {
+      document.querySelector("#password").value = "Please determine a correct password length and select at least one character type(s) option!";
+    }
   }
-  else {
-    document.querySelector("#password").value = "Please determine password length and select at least one character type(s) option!";
-  }
+  else {}
 }
-  
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-
-
-
