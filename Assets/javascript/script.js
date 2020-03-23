826550f3460fc923b4819dc83aad7763ec6c3d37
Norberto Mantohac <nmantohac82098@gmail.com>
2020-03-22 18:34:20 -0700

    completed password generator with initial javascript

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,14 +1,63 @@
-// Assignment Code
+var passwordLength = document.querySelector("#password-length-input");
+var lowercaseCheckBox = document.querySelector("#lowercase-checkbox");
+var uppercaseCheckBox = document.querySelector("#uppercase-checkbox");
+var numbersCheckBox = document.querySelector("#numbers-checkbox");
+var symbolsCheckBox = document.querySelector("#symbols-checkbox");
+// var passwordResult = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+const passwordCriteria = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numbers: "0123456789",
+  symbols: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+};
 
-  passwordText.value = password;
+generateBtn.addEventListener("click", () => {
+  var length = +passwordLength.value;
+  var lowercaseActive = lowercaseCheckBox.checked;
+  var uppercaseActive = uppercaseCheckBox.checked;
+  var numbersActive = numbersCheckBox.checked;
+  var symbolsActive = symbolsCheckBox.checked;
 
+  generateRandomPassword(lowercaseActive, uppercaseActive, numbersActive, symbolsActive, length);
+});
+
+function generateRandomPassword(lower, upper, num, sym, length) {
+  let mainString = "";
+  let password = "";
+
+  const options = {
+    lowercase: lower,
+    uppercase: upper,
+    numbers: num,
+    symbols: sym
+  };
+
+  for (var i = 0; i < Object.keys(options).length; i++) {
+    mainString += (Object.values(options)[i]) ? passwordCriteria[Object.keys(options)[i]] : "";
+  }
+
+  if (mainString != "" && length >= 8 && length <= 128) {
+    for (var j = 0; j < length; j++) {
+      password += mainString[Math.floor(Math.random() * mainString.length)];
+    }
+
+    document.querySelector("#password").value = password;
+  }
+  else {
+    document.querySelector("#password").value = "Please determine password length and select at least one character type(s) option!";
+  }
 }
+  
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//   passwordText.value = password;
+
+// }
+
+
+
+
